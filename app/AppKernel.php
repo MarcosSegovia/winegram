183bb982333d5f3e0b4d262b21b51cdda8960eef
Marcos Segovia <velozmarkdrea@gmail.com>
2016-04-09 20:30:07 +0200

    Requiring Hilazo SQS Bundle and Marcos Twitter API bundle and defining minimum parameters to work

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new SocialDataPoolBundle\Bundle\SocialDataPoolBundle(),
+            new SqsPhpBundle\SqsPhpBundle(),
+            new MarcosSegovia\TwitterApiClientBundle\TwitterApiClientBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
