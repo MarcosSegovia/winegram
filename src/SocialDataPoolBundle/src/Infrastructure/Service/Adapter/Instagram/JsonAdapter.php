3b878703367b4a9230211a0bb7ed2601ebb39be7
Marcos <velozmarkdrea@gmail.com>
2016-06-21 21:32:43 +0200

    changing order

--- src/SocialDataPoolBundle/src/Infrastructure/Service/Adapter/Instagram/JsonAdapter.php
+++ src/SocialDataPoolBundle/src/Infrastructure/Service/Adapter/Instagram/JsonAdapter.php
@@ -14,26 +14,25 @@ final class JsonAdapter
         $a_product_id = null
     )
     {
-        $this->my_array_of_social_data = [];
+        $this->my_array_of_social_data          = [];
+        $this->my_array_of_social_data['id']    = $an_instagram_post_response_to_adapt->id;
+        $this->my_array_of_social_data['type']  = Post::INSTAGRAM_TYPE;
+        $this->my_array_of_social_data['text']  = $an_instagram_post_response_to_adapt->caption->text;
+        $this->my_array_of_social_data['username'] = $an_instagram_post_response_to_adapt->caption->from->full_name;
+        $this->my_array_of_social_data['media'] = $an_instagram_post_response_to_adapt->images->standard_resolution->url;
+        $this->my_array_of_social_data['likes_count'] = $an_instagram_post_response_to_adapt->likes->count;
 
-        $this->my_array_of_social_data['id']                      = $an_instagram_post_response_to_adapt->id;
-        $this->my_array_of_social_data['type']                    = Post::INSTAGRAM_TYPE;
-        $this->my_array_of_social_data['text']                    = $an_instagram_post_response_to_adapt->caption->text;
-        $this->my_array_of_social_data['url_images']['low']       = $an_instagram_post_response_to_adapt->images->low_resolution->url;
-        $this->my_array_of_social_data['url_images']['thumbnail'] = $an_instagram_post_response_to_adapt->images->thumbnail->url;
-        $this->my_array_of_social_data['url_images']['standard']  = $an_instagram_post_response_to_adapt->images->standard_resolution->url;
-        $this->my_array_of_social_data['likes']                   = $an_instagram_post_response_to_adapt->likes->count;
+        foreach ($an_instagram_post_response_to_adapt->tags as $current_hashtag)
+        {
+            $this->my_array_of_social_data['tags'][] = $current_hashtag;
+        }
 
         if (null !== $a_product_id)
         {
             $this->my_array_of_social_data['uvinum_product_id'] = $a_product_id;
         }
-        
-        foreach ($an_instagram_post_response_to_adapt->tags as $current_hashtag)
-        {
-            $this->my_array_of_social_data['tags'][] = $current_hashtag;
-        }
-        $this->my_array_of_social_data['username'] = $an_instagram_post_response_to_adapt->caption->from->full_name;
+
+
 
         return $this->encodePostData();
     }
--- src/SocialDataPoolBundle/src/Infrastructure/Service/Adapter/Twitter/JsonAdapter.php
+++ src/SocialDataPoolBundle/src/Infrastructure/Service/Adapter/Twitter/JsonAdapter.php
@@ -9,26 +9,30 @@ final class JsonAdapter
     /** @var array */
     private $my_array_of_social_data;
 
-    public function __invoke($a_twitter_response_to_adapt, $a_product_id = null)
+    public function __invoke(
+        $a_twitter_response_to_adapt,
+        $a_product_id = null
+    )
     {
         $this->my_array_of_social_data = [];
 
         $this->my_array_of_social_data['id']             = $a_twitter_response_to_adapt['id_str'];
         $this->my_array_of_social_data['type']           = Tweet::TWITTER_TYPE;
         $this->my_array_of_social_data['text']           = $a_twitter_response_to_adapt['text'];
-        $this->my_array_of_social_data['retweet_count']  = $a_twitter_response_to_adapt['retweet_count'];
-        $this->my_array_of_social_data['favorite_count'] = $a_twitter_response_to_adapt['favorite_count'];
-
+        $this->my_array_of_social_data['username'] = $a_twitter_response_to_adapt['user']['name'];
+        $this->my_array_of_social_data['likes_count'] = $a_twitter_response_to_adapt['favorite_count'];
+        
+        foreach ($a_twitter_response_to_adapt['entities']['hashtags'] as $current_hashtag)
+        {
+            $this->my_array_of_social_data['tags'][] = $current_hashtag['text'];
+        }
+        
         if (null !== $a_product_id)
         {
             $this->my_array_of_social_data['uvinum_product_id'] = $a_product_id;
         }
 
-        foreach ($a_twitter_response_to_adapt['entities']['hashtags'] as $current_hashtag)
-        {
-            $this->my_array_of_social_data['tags'][] = $current_hashtag['text'];
-        }
-        $this->my_array_of_social_data['username'] = $a_twitter_response_to_adapt['user']['name'];
+
 
         return $this->encodeTweetData();
     }
